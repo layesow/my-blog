9d98ec0eff471397fd3f624899b0d0d4c61cc88f
Laye-Sow <abdoulayesow716@gmail.com>
2023-11-11 23:40:44 +0000

    ajout de du chargement de la page

--- src/Home.js
+++ src/Home.js
@@ -2,9 +2,12 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 const Home = () => {
     
-
     // eslint-disable-next-line no-unused-vars
     const [blogs, setBlog]= useState( null );
+    //loading
+    const [isLoading, setIsLoading] = useState(true);
+  
+    //error
 
     useEffect( ()=>{
         //fetch pour la recuperation de donnee via une api
@@ -14,12 +17,15 @@ const Home = () => {
             })
             .then((data)=>{
                 setBlog(data);
+                setIsLoading(false);
+                
             })
     },[] );
 
     return ( 
         <div className="home">
-            
+            {/* si le loading est true on affiche un loader sinon on affiche les blogs */}
+            { isLoading && <div>En chargement... </div> }
             {blogs && <BlogList blogs={blogs} title />}
 
         </div>
