fe2d998e4a9e46232313ceb90516d10e7edc293e
Laye-Sow <abdoulayesow716@gmail.com>
2023-11-12 00:13:56 +0000

    gerer les erreur de mon app

--- src/Home.js
+++ src/Home.js
@@ -6,6 +6,9 @@ const Home = () => {
     const [blogs, setBlog]= useState( null );
     //loading
     const [isLoading, setIsLoading] = useState(true);
+
+    // erreur usestate
+    const [error, setError] = useState(null)
   
     //error
 
@@ -13,17 +16,31 @@ const Home = () => {
         //fetch pour la recuperation de donnee via une api
         fetch('http://localhost:8000/blogs')
             .then((response)=> {
+                //error
+                if(!response.ok) {
+                    throw  Error('Desolé une erreur est survenue ...');
+                }
                 return response.json();
             })
             .then((data)=>{
                 setBlog(data);
                 setIsLoading(false);
+                setError(null);
                 
             })
+            //erreur
+            .catch((err) => {
+                console.log(err.message);
+                setError(err.message);
+                setIsLoading(false);
+            });
+        
     },[] );
 
     return ( 
         <div className="home">
+            {/* error */}
+            {error && <div style={ {'color': 'red'} }>{error}</div>}
             {/* si le loading est true on affiche un loader sinon on affiche les blogs */}
             { isLoading && <div>En chargement... </div> }
             {blogs && <BlogList blogs={blogs} title />}
